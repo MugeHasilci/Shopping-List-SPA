2a475b9183ec167f9858e3e6ecf21b7502d37680
muge <muge.hasilci@hotmail.com>
2018-11-17 21:31:01 +0000

    when user enters a new item, item is appearing on the list

--- app.js
+++ app.js
@@ -16,6 +16,21 @@ function addItem(){
   newItem.name = $('#newItemName').val();
   currentList.items.push(newItem);
   console.log(currentList);
+  drawItems();
+}
+
+function drawItems() {
+  var $list = $('#shoppingListItems').empty();
+
+  for(var i = 0; i < currentList.items.length; i++) {
+    var currentItem = currentList.items[i];
+    var $li = $("<li>").html(currentItem.name)
+    .attr("id", "item_" + i);
+    var $deleteBtn = $("<button>delete</button>").appendTo($li);
+    var $checkBtn = $("<button>check</button>").appendTo($li);
+
+    $li.appendTo($list);
+  }
 }
 
 
